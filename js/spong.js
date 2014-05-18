1018c78e659d900824b7612e7d9c66cc5c046199
rtorres <raymondjavaxx@gmail.com>
2014-05-17 21:00:49 -0400

    Refactoring

--- js/spong.js
+++ js/spong.js
@@ -28,9 +28,12 @@ var Spong = (function(){
 	var screenH;
 
 	var Sounds = (function() {
-		var bop = new Audio('sfx/bop.wav');
+		var bop  = new Audio('sfx/bop.wav');
 		var beep = new Audio('sfx/beep.wav');
-		return {bop:bop, beep:beep};
+		return {
+			bop:bop,
+			beep:beep
+		};
 	})();
 
 	var keyboardState = {up:false, down:false};
@@ -42,7 +45,7 @@ var Spong = (function(){
 
 	var players = {one:{score:3}, two:{score:0}};
 
-	var ball = {x:10, y:10, w:16, h:16, velocity:{x:10, y:10}};
+	var ball = {x:10, y:10, w:16, h:16, velocity:{x:5, y:5}};
 	ball.collidesWith = function(pad) {
 		if ((this.x + this.w >= pad.x) &&
 		    (this.x <= pad.x + pad.w) &&
@@ -75,36 +78,37 @@ var Spong = (function(){
 			for (var y = bitmap.length - 1; y >= 0; y--){
 				for (var x = bitmap[y].length - 1; x >= 0; x--){
 					if (bitmap[y][x] == 1) {
-						ctx.fillRect(offsetX + x*16, offsetY + y*16, 16, 16);
+						//ctx.fillRect(offsetX + x*16, offsetY + y*16, 16, 16);
 					}
 				};
 			};
 		};
 
 		var clear = function() {
-			ctx.fillStyle = 'rgb(0, 0, 0)';
+			ctx.fillStyle = '#000';
 			ctx.fillRect(0, 0, canvas.width, canvas.height);
 		};
 
 		var drawPads = function() {
-			ctx.fillStyle = 'rgb(255, 255, 255)';
+			ctx.fillStyle = '#fff';
 			ctx.fillRect(pads.one.x, pads.one.y, pads.one.w, pads.one.h);
 			ctx.fillRect(pads.two.x, pads.two.y, pads.two.w, pads.two.h);
 		};
 
 		var drawBall = function() {
-			ctx.fillStyle = 'rgb(255, 255, 255)';
+			ctx.fillStyle = '#fff';
 			ctx.fillRect(ball.x, ball.y, 16, 16);
 		};
 
 		var drawNet = function() {
 			var x = ((screenW / 2) - 4);
 			for (var y = (screenH - 64); y >= 0; y-=48){
-				ctx.fillRect(x, y, 8, 24);
+				//ctx.fillRect(x, y, 8, 24);
 			}
 		};
 
 		var drawScanlines = function() {
+			ctx.save();
 			ctx.beginPath();
 	
 			for (var y=0.4; y < screenH; y+=2) {
@@ -114,8 +118,28 @@ var Spong = (function(){
 
 			ctx.lineWidth = 1;
 			ctx.lineCap = 'butt';
-			ctx.strokeStyle = 'rgb(0, 0, 0)';
+			ctx.strokeStyle = '#000';
 			ctx.stroke();
+			ctx.restore();
+		};
+
+		var shader = function () {
+			var offset;
+			var data = ctx.getImageData(0, 0, screenW, screenH);
+
+			for (var x = 0; x < data.width; x+=1) {
+				for (var y = 0; y < data.height; y+=1) {
+					offset = ((x*4) + (y * screenW * 4));
+					if ((y % 2) === 0) {
+						data.data[offset + 0] = data.data[offset + 0] * 0.5;
+						data.data[offset + 1] = data.data[offset + 1] * 0.5;
+						data.data[offset + 2] = data.data[offset + 2] * 0.5;
+						data.data[offset + 3] = data.data[offset + 3] * 0.5;
+					}
+				};
+			};
+
+			ctx.putImageData(data, 0, 0);
 		};
 
 		var draw = function() {
@@ -125,7 +149,8 @@ var Spong = (function(){
 			drawNet();
 			drawNumber(players.one.score, ((screenW / 2) - 4) - 240, 30);
 			drawNumber(players.two.score, ((screenW / 2) - 4) + 176, 30);
-			drawScanlines();
+			//drawScanlines();
+			//shader();
 		};
 
 		return {draw:draw};
@@ -170,11 +195,14 @@ var Spong = (function(){
 		};
 
 		// Game loop
-		setInterval(function(){
+		var doFrame = function (time) {
 			handleInput();
 			doPhysics();
 			Graphics.draw();
-		}, 1000.0 / 60.0);
+			window.requestAnimationFrame(doFrame);
+		};
+
+		window.requestAnimationFrame(doFrame);
 	};
 
 	var handleInput = function() {
@@ -196,28 +224,28 @@ var Spong = (function(){
 
 		if (ball.y >= screenH - ball.h) {
 			ball.velocity.y *= -1;
-			Sounds.bop.play();
+			//Sounds.bop.play();
 		}
 
 		if (ball.x >= screenW - ball.h) {
 			ball.velocity.x *= -1;
-			Sounds.bop.play();
+			//Sounds.bop.play();
 		}
 
 		if (ball.y <= 0) {
 			ball.velocity.y *= -1;
-			Sounds.bop.play();
+			//Sounds.bop.play();
 		}
 
 		if (ball.x <= 0) {
 			ball.velocity.x *= -1;
-			Sounds.bop.play();
+			//Sounds.bop.play();
 		}
 
 		if (ball.collidesWith(pads.one) || ball.collidesWith(pads.two)) {
 			ball.velocity.x *= -1;
 			//ball.velocity.y *= -1;
-			Sounds.beep.play();
+			//Sounds.beep.play();
 		}
 
 		ball.x += ball.velocity.x;
